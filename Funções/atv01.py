92eea1f42759272576ed288cf6748414775857a8
laicts <laisvictoria.cts@gmail.com>
2024-01-19 14:43:22 -0300

    add atividade

---
+++ Funções/atv01.py
@@ -0,0 +1,10 @@
+# Faça um programa, com uma função que necessite de três
+# argumentos, e que forneça a soma desses três argumentos.
+
+def soma(num1, num2, num3):
+    calculo = num1 + num2 + num3
+    print(f'O resultado é: {calculo}')
+
+
+
+soma(20, 30, 40)
