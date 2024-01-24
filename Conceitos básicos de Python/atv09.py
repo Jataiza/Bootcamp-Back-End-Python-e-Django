e5352b59088140bc011b8b97659378f940fda9aa
julianapequeno <julianafpsc@gmail.com>
2024-01-24 17:34:49 -0300

    feat: resolucao do exercicio 9 da lista 1

--- Conceitos básicos de Python/atv09.py
+++ Conceitos básicos de Python/atv09.py
@@ -1,1 +1,12 @@
 
+horas_exercicio = int(
+    input('Digite o número de horas que você se exercita na semana: '))
+
+
+minutos_de_exercicio_por_semana = horas_exercicio*60
+calorias_por_minuto = 5
+semanas_no_mes = 4
+
+calorias = (minutos_de_exercicio_por_semana*calorias_por_minuto)*semanas_no_mes
+
+print('Você gasta ',calorias," calorias em 1 mês")
