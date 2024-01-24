dbcaebe863dbc0868ac6de3225a04af810294f9c
julianapequeno <julianafpsc@gmail.com>
2024-01-24 17:24:03 -0300

    feat: resolucao do exercicio 3 da lista 1

--- Conceitos básicos de Python/atv03.py
+++ Conceitos básicos de Python/atv03.py
@@ -1,5 +1,12 @@
-#Faça um Programa que peça a quantidade de quilômetros, transforme em metros, centímetros e milímetros.
+# Faça um Programa que peça a quantidade de quilômetros, transforme em metros, centímetros e milímetros.
+
+km = float(input('Digite a quantidade de quilômetros: '))
 
 
+metros = km*1000
+centimetros = metros*100
+milimetros = centimetros*10
 
 
+print(km, ' KM é correspondente a: ', metros, ' metros, ',
+      centimetros, ' centimetros, ', milimetros, 'milimetros')
