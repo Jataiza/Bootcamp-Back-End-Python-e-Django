701535a105fd617b63c38e5b0e00745fced5b54c
Jataiza <85134137+Jataiza@users.noreply.github.com>
2024-01-25 20:39:51 -0300

    Entrega atv10.py

--- Conceitos básicos de Python/atv10.py
+++ Conceitos básicos de Python/atv10.py
@@ -1,1 +1,13 @@
+''' Faça um Programa que utilize 4 variáveis como preferir e no final print uma mensagem amigável utilizando as variáveis criadas. 
+Exemplos de variáveis: nome, idade, lugar, profissão .... Exemplo de retorno: Olá Maria, prazer te conhecer. 
+Sou de São Paulo também e estou migrando de área.
+O código esta certo 
+'''
+#Declaração variáveis
+nome = 'Jataiza'
+idade = '20'
+cidade = 'Belo Horizonte'
+animal_favorito = 'Cachorro'
 
+#Exibindo a mensangem na tela 
+print(f"Olá {nome}, prazer te conhecer. Sou de {cidade} meu animal favorito é {animal_favorito}.")
