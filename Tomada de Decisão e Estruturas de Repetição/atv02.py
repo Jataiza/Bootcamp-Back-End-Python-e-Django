52e6741e0a715b4440b3e0f226f72f455782df8a
Jataiza <85134137+Jataiza@users.noreply.github.com>
2024-01-26 09:38:46 -0300

    atv02.py

---
+++ Tomada de Decisão e Estruturas de Repetição/atv02.py
@@ -0,0 +1,9 @@
+#Faça um programa que peça dois números e imprima o maior deles
+
+numero1 =int(input("Digite um numero:"))
+numero2=int(input("Digite um numero:"))
+
+if numero1 >numero2:
+    print("O numero 1º é maior")
+else:
+    print("O numero 2º é maior")
